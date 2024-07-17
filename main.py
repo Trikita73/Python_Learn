83b4fe3e177f5b37b94e9483aedf49f4a1c64b37
Trikita73 <diachenkoandrii7@gmail.com>
2024-07-17 13:26:32 +0300

    first_less

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello World') 
+#first lesson
+
+'''
+x = 2 
+y = 3 
+z = x + y 
+print(z)
+'''
+
+'''
+greetings = "Hello World"
+print(greetings)
+'''
+
+'''
+age = 33
+print("my_age_is:", age)
+'''
